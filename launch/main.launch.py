aa919b72c1b2cd0a00d9b5df194321dab8a200ad
Siddhesh Phadke <siddheshsp0@gmail.com>
2025-06-13 01:43:26 +0530

    Added rviz configuration and added rviz2 launch description in the launch file.

--- launch/main.launch.py
+++ launch/main.launch.py
@@ -1,10 +1,26 @@
 # launch/launch_perception.launch.py
 from launch import LaunchDescription
 from launch_ros.actions import Node
+import os
+from ament_index_python.packages import get_package_share_directory
+
 
 def generate_launch_description():
     package_name = "perception_winter"
+    rviz_config_path = os.path.join(
+        get_package_share_directory('perception_winter'),
+        'rviz_config',
+        'config.rviz'
+    )
     return LaunchDescription([
+        # Launch rviz
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_config_path],
+            output='screen'
+        ),
         # Running the process_lidar node
         Node(
             package=package_name,        # Replace with your actual package name
